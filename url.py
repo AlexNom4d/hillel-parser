26d85e2f71460bb294eb6e9ce83a5a7a30ce98bd
Alex <94796948+AlexNom4d@users.noreply.github.com>
2021-11-28 20:33:15 +0200

    Add files via upload

---
+++ url.py
@@ -0,0 +1,32 @@
+from urllib.parse import urlparse
+
+url = 'https://lms.ithillel.ua/groups/612355f386b02c38d884ecf4/homeworks/61995aa4e1fae77747ae897f'
+t = urlparse(url)
+
+url = 'https://wiki.paparazziuav.org/wiki/Github_manual_for_Ubuntu'
+t = urlparse(url)
+
+url = 'https://www.facebook.com/'
+t = urlparse(url)
+
+url = 'https://www.youtube.com/watch?v=s9HU0v0bgtQ'
+t = urlparse(url)
+
+url = 'https://www.instagram.com/'
+t = urlparse(url)
+
+url = 'https://odessa.ithillel.ua/courses/testing'
+t = urlparse(url)
+
+url = 'https://blog.ithillel.ua/publications/devops'
+t = urlparse(url)
+
+url = 'https://dnipro.ithillel.ua/employment'
+t = urlparse(url)
+
+url = 'https://dnipro.ithillel.ua/courses/python-pro'
+t = urlparse(url)
+
+url = 'https://ithillel.ua/contact'
+t = urlparse(url)
+
